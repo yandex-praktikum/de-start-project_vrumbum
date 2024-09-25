5bb061fe7a24698e3b6b072e360c1d96db116f5d
Alexandr <80157830+wucrew2k1@users.noreply.github.com>
2024-09-25 23:26:16 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,15 @@
-/*добавьте сюда запрос для решения задания 4*/
+SELECT 
+    p.name AS person,
+    STRING_AGG(CONCAT(b.name, ' ', c.model), ', ') AS cars
+FROM 
+    car_shop.purchases pu
+JOIN 
+    car_shop.people p ON pu.person_id = p.id
+JOIN 
+    car_shop.cars c ON pu.car_id = c.id
+JOIN 
+    car_shop.brands b ON c.brand_id = b.id
+GROUP BY 
+    p.name
+ORDER BY 
+    p.name ASC;/*добавьте сюда запрос для решения задания 4*/
