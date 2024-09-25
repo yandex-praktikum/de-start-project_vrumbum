ba593d4a3310cfa6cbe231ad7c366faa4d407334
Alexandr <80157830+wucrew2k1@users.noreply.github.com>
2024-09-25 23:25:38 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,12 @@
-/*добавьте сюда запрос для решения задания 3*/
+SELECT 
+    EXTRACT(MONTH FROM s.sale_date) AS month,
+    2022 AS year,
+    ROUND(AVG(s.price - (s.price * s.discount / 100)), 2) AS price_avg
+FROM 
+    car_shop.sales s
+WHERE 
+    EXTRACT(YEAR FROM s.sale_date) = 2022
+GROUP BY 
+    month, year
+ORDER BY 
+    month ASC;/*добавьте сюда запрос для решения задания 3*/
