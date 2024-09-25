252d54e6eeda8048ec0b4784e5d357ee921ba1d4
Alexandr <80157830+wucrew2k1@users.noreply.github.com>
2024-09-25 23:25:10 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,1 +1,14 @@
-/*добавьте сюда запрос для решения задания 2*/
+SELECT 
+    b.name AS brand_name,
+    EXTRACT(YEAR FROM s.sale_date) AS year,
+    ROUND(AVG(s.price - (s.price * s.discount / 100)), 2) AS price_avg
+FROM 
+    car_shop.sales s
+JOIN 
+    car_shop.cars c ON s.car_id = c.id
+JOIN 
+    car_shop.brands b ON c.brand_id = b.id
+GROUP BY 
+    b.name, year
+ORDER BY 
+    b.name ASC, year ASC;/*добавьте сюда запрос для решения задания 2*/
