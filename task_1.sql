53ac25a5a73a80df7222a1fc11c389f53bbf04da
Alexandr <80157830+wucrew2k1@users.noreply.github.com>
2024-09-25 23:24:32 +0300

    Update task_1.sql

--- task_1.sql
+++ task_1.sql
@@ -1,1 +1,5 @@
-/*добавьте сюда запрос для решения задания 1*/
+SELECT 
+    (COUNT(*) FILTER (WHERE gasoline_consumption IS NULL)::DECIMAL / 
+     COUNT(*)) * 100 AS nulls_percentage_gasoline_consumption
+FROM 
+    raw_data.sales;/*добавьте сюда запрос для решения задания 1*/
