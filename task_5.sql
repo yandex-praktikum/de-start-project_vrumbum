01e6149afcdad2dcc61f43137956e4e45c2f3d60
Alexandr <80157830+wucrew2k1@users.noreply.github.com>
2024-09-25 23:26:43 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,1 +1,14 @@
-/*добавьте сюда запрос для решения задания 5*/
+SELECT 
+    b.origin AS brand_origin,
+    MAX(s.price - (s.price * s.discount / 100)) AS price_max,
+    MIN(s.price - (s.price * s.discount / 100)) AS price_min
+FROM 
+    car_shop.sales s
+JOIN 
+    car_shop.cars c ON s.car_id = c.id
+JOIN 
+    car_shop.brands b ON c.brand_id = b.id
+GROUP BY 
+    b.origin
+ORDER BY 
+    b.origin;/*добавьте сюда запрос для решения задания 5*/
